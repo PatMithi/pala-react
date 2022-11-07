4c1bf10d813ac01751b07cdc673c3c65d84f981c
PatMithi <Patrickmithi@gmail.com>
2022-11-07 18:04:29 +0000

    loading-message-and-backend

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,39 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import React, { useEffect, useReducer, useState } from "react";
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true};
+        case 'FETCH_SUCCESS':
+            return {...state, products:action.payload, loading: false};
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
 
 function Products () {
-    const [products, setProducts] = useState([]);
+    const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+        products: [], 
+        loading: true, 
+        error: '',
+    })
     useEffect(() => {
+        
         const fetchData = async() => {
-            const result = await axios.get('/api/products');
-            setProducts(result.data);
+            dispatch({type: 'FETCH_REQUEST'});
+            try {
+               const result = await axios.get('/api/products'); 
+               dispatch({type: 'FETCH_SUCCESS', payload: result.data});
+            }
+            catch(err) {
+                dispatch({type: 'FETCH_FAIL', payload: err.message});
+            }
+            
+            // setProducts(result.data);
         };
         fetchData();
     }, []);
@@ -17,6 +44,10 @@ function Products () {
                 <h1>Featured Products</h1>                
                 <div className="products">
                 {
+                    loading? (<div>Loading...</div>
+                )   :
+                    error? (<div>{error}</div>
+                )   : (
                     products.map(product => (
                     <div key={product.image} className="product">
                         <img src={product.image} alt={product.name}/>
@@ -28,6 +59,7 @@ function Products () {
                         </div>
                     </div>
                     ))
+                )
                 }
                 </div>
                 
